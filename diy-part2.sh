d021e719bbed5b039b8bc3b80571cf440c4e1f1f
pianranjinghong <806481345@qq.com>
2022-01-03 10:04:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,16 +12,10 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.4/g' package/base-files/files/bin/config_generate   # 修改默认ip
-#sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd    # 替换终端为bash
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile   # 选择argon为默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile   # 选择argon为默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-ssl-nginx/Makefile   # 选择argon为默认主题
-#rm -rf package/lean/luci-theme-argon    # 删除自带argon
 
-#rm -rf openwrt-packages-backup/luci/applications/luci-app-passwall
-#rm -rf openwrt-packages-backup/luci/applications/luci-app-unblockneteasemusic
-#rm -rf openwrt-packages-backup/packages
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon    # 替换新版argon
 sed -i 's/+uhttpd +uhttpd-mod-ubus //g' feeds/luci/collections/luci/Makefile    # 删除uhttpd
 #sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings    # 设置密码为空
 #sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/x86/Makefile   # x86机型,默认内核5.4，修改内核为5.10
