63012c5ea9ab5d6a5f8ea4848f711faff590e02b
pianranjinghong <806481345@qq.com>
2022-07-25 05:46:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 git clone https://github.com/sundaqiang/openwrt-packages package/sundaqiang
-git clone https://github.com/QiuSimons/openwrt-mos package/QiuSimons/openwrt-modns
+git clone https://github.com/QiuSimons/openwrt-mos package/QiuSimons/openwrt-mosdns
 #git clone https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
 #git clone https://github.com/sbwml/v2ray-geodata package/sbwml/luci-app-mosdns/v2ray-geodata
 git clone https://github.com/kiddin9/openwrt-bypass package/kiddin9/openwrt-bypass
