9575134555ceddebe7c493e8524587ce98d04e49
pianranjinghong <806481345@qq.com>
2024-02-11 15:40:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,13 +19,13 @@
 #git clone https://github.com/sirpdboy/luci-app-advanced package/sirpdboy/luci-app-advanced
 #git clone https://github.com/yichya/luci-app-xray package/yichya/luci-app-xray
 
-rm -rf feeds/luci/themes/luci-theme-edge
+
 rm -rf feeds/luci/themes/luci-theme-argon
 
 git clone https://github.com/kiddin9/luci-theme-edge package/kiddin9/luci-theme-edge
-git clone -b js https://github.com/gngpp/luci-theme-design feeds/luci/themes/luci-theme-design
+git clone -b js https://github.com/gngpp/luci-theme-design package/gngpp/luci-theme-design
 git clone https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
-git clone -b js https://github.com/sirpdboy/luci-theme-kucat package/kiddin9/luci-theme-kucat
+git clone -b js https://github.com/sirpdboy/luci-theme-kucat package/sirpdboy/luci-theme-kucat
 
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/luci/applications/luci-app-smartdns
@@ -37,7 +37,7 @@ git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodat
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/messense/aliyundrive-webdav
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/xiaorouji/openwrt-passwall2
 git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
-git clone https://github.com/kiddin9/luci-app-advancedplus package/kiddin9/luci-app-advancedplus
+git clone https://github.com/sirpdboy/luci-app-advancedplus package/sirpdboy/luci-app-advancedplus
 
 # curl/8.5.0 - fix passwall `time_pretransfer` check
 rm -rf feeds/packages/net/curl
