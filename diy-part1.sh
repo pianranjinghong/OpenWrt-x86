779091b6322ff1d038be68b70a07a8ab1b7a5c0b
pianranjinghong <806481345@qq.com>
2024-02-13 20:01:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,7 +28,7 @@ git clone https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-t
 git clone -b js https://github.com/sirpdboy/luci-theme-kucat package/sirpdboy/luci-theme-kucat
 
 rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/luci/applications/luci-app-smartdns
+#rm -rf feeds/luci/applications/luci-app-smartdns
 rm -rf feeds/packages/net/mosdns
 
 git clone -b openwrt https://github.com/pianranjinghong/openwrt-packages package/pianranjinghong
@@ -36,7 +36,7 @@ git clone https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
 git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/messense/aliyundrive-webdav
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/xiaorouji/openwrt-passwall2
-git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
+#git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/sirpdboy/luci-app-advancedplus package/sirpdboy/luci-app-advancedplus
 
 # curl/8.5.0 - fix passwall `time_pretransfer` check
