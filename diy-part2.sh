69717dd37049e70612705530c95843f2bf6c737e
pianranjinghong <806481345@qq.com>
2024-02-13 20:02:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,8 +17,8 @@ sed -i 's/ImmortalWrt/OpenWrt/g' include/version.mk    # 修改版本名
 #sed -i 's/geoip-only-cn-private.dat/geoip.dat/g' package/QiuSimons/openwrt-mosdns/v2ray-geodata/Makefile   # 修改geoip.dat
 sed -i '16s/1/8/' feeds/luci/applications/luci-app-dockerman/luasrc/controller/dockerman.lua
 sed -i 's/geoip-only-cn-private.dat/geoip.dat/g' feeds/packages/net/v2ray-geodata/Makefile   # 修改geoip.dat
-sed -i 's/43/45/' feeds/packages/net/smartdns/Makefile
-sed -i 's/2a5e7869603ecb7b0d94e153d938e798f0b8f260cc6062cc095a39116386d8b3/b17d82738f3ae73f5a60ad25c824e1000c05a6d060d08ebd1ec295a2caa5b495/' feeds/packages/net/smartdns/Makefile 
+#sed -i 's/43/45/' feeds/packages/net/smartdns/Makefile
+#sed -i 's/2a5e7869603ecb7b0d94e153d938e798f0b8f260cc6062cc095a39116386d8b3/b17d82738f3ae73f5a60ad25c824e1000c05a6d060d08ebd1ec295a2caa5b495/' feeds/packages/net/smartdns/Makefile 
 #修改ddns启动，解决重启不能运行问题
 
 sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile   # 选择argon为默认主题
