5f06ec7728d772f343fa7084bde902ac3db90969
pianranjinghong <806481345@qq.com>
2024-12-08 13:40:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,17 +26,21 @@ git clone https://github.com/kiddin9/luci-theme-edge package/kiddin9/luci-theme-
 git clone https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 #git clone -b js https://github.com/sirpdboy/luci-theme-kucat package/sirpdboy/luci-theme-kucat
 
-#rm -rf feeds/packages/net/v2ray-geodata
-#mv feeds/luci/applications/luci-app-passwall feeds/luci/applications/luci-app-passwall1
-rm -rf feeds/luci/applications/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
-#git clone -b luci-smartdns-dev https://github.com/lwb1978/openwrt-passwall feeds/luci/applications/luci-app-passwall
-#rm -rf feeds/luci/applications/luci-app-smartdns
-#rm -rf feeds/packages/net/mosdns
+# ------------------PassWall 科学上网--------------------------
+# 移除 openwrt feeds 自带的核心库
+#rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,pdnsd-alt,brook,chinadns-ng,dns2socks,dns2tcp,gn,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan,trojan-go,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,gn}
+# 核心库
+#git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+#rm -rf package/passwall-packages/{chinadns-ng,naiveproxy,shadowsocks-rust,v2ray-geodata}
+#merge_package v5 https://github.com/sbwml/openwrt_helloworld package/passwall-packages chinadns-ng naiveproxy shadowsocks-rust v2ray-geodata
 
+rm -rf feeds/packages/net/{v2ray-geodata,mosdns}
+rm -rf feeds/luci/applications/luci-app-passwall
+
+git clone https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
 git clone -b openwrt https://github.com/pianranjinghong/openwrt-packages package/pianranjinghong
 git clone -b v5 https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
-#git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata feeds/packages/net/v2ray-geodata
 #git clone https://github.com/xiaorouji/openwrt-passwall2 package/xiaorouji/openwrt-passwall2
 #git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
 #git clone https://github.com/sirpdboy/luci-app-advancedplus package/sirpdboy/luci-app-advancedplus
