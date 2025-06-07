e0297dc8cf4d365ce2e55f6d6295634e89c818b1
pianranjinghong <806481345@qq.com>
2025-06-07 11:09:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -32,13 +32,13 @@ git clone https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-t
 # 核心库
 #git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 
-rm -rf feeds/luci/applications/luci-app-passwall
+#rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/packages/net/{v2ray-geodata,mosdns}
 rm -rf feeds/packages/net/smartdns
 
 git clone https://github.com/pianranjinghong/openwrt-smartdns.git feeds/packages/net/smartdns
 git clone https://github.com/pianranjinghong/v2ray-geodata feeds/packages/net/v2ray-geodata
-git clone https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/xiaorouji/luci-app-passwall2
 git clone -b openwrt https://github.com/pianranjinghong/openwrt-packages package/pianranjinghong
 git clone -b v5 https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
