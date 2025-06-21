4125344e9e9e8ec9bdd1537c95238fba3718d93b
pianranjinghong <806481345@qq.com>
2025-06-21 16:32:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@
 #git clone https://github.com/yichya/luci-app-xray package/yichya/luci-app-xray
 
 
-rm -rf feeds/luci/themes/luci-theme-argon
+#rm -rf feeds/luci/themes/luci-theme-argon
 
 git clone https://github.com/kiddin9/luci-theme-edge package/kiddin9/luci-theme-edge
 #git clone https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
@@ -36,12 +36,12 @@ git clone https://github.com/kiddin9/luci-theme-edge package/kiddin9/luci-theme-
 rm -rf feeds/packages/net/{v2ray-geodata,mosdns}
 rm -rf feeds/packages/net/smartdns
 
-git clone https://github.com/pymumu/openwrt-smartdns.git package/pymumu/smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
 git clone https://github.com/pianranjinghong/v2ray-geodata feeds/packages/net/v2ray-geodata
 #git clone https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/xiaorouji/luci-app-passwall2
 git clone -b openwrt https://github.com/pianranjinghong/openwrt-packages package/pianranjinghong
-git clone -b v5 https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
+#git clone -b v5 https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
 
 rm -rf package/xiaorouji/luci-app-passwall2/luci-app-passwall2/htdocs
 #git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
