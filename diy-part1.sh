3008c92a5d561db29306441a58946a68f96614d8
pianranjinghong <806481345@qq.com>
2025-07-02 14:01:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,13 +15,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-
-#git clone https://github.com/sirpdboy/luci-app-advanced package/sirpdboy/luci-app-advanced
-#git clone https://github.com/yichya/luci-app-xray package/yichya/luci-app-xray
-
-
-#rm -rf feeds/luci/themes/luci-theme-argon
-
 git clone https://github.com/kiddin9/luci-theme-edge package/kiddin9/luci-theme-edge
 #git clone https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
 #git clone -b js https://github.com/sirpdboy/luci-theme-kucat package/sirpdboy/luci-theme-kucat
@@ -35,6 +28,7 @@ git clone https://github.com/kiddin9/luci-theme-edge package/kiddin9/luci-theme-
 #rm -rf feeds/luci/applications/luci-app-passwall
 rm -rf feeds/packages/net/{v2ray-geodata,mosdns}
 rm -rf feeds/packages/net/smartdns
+rm -rf feeds/luci/applications/luci-app-smartdns
 
 git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
 git clone https://github.com/pianranjinghong/v2ray-geodata feeds/packages/net/v2ray-geodata
@@ -44,7 +38,6 @@ git clone -b openwrt https://github.com/pianranjinghong/openwrt-packages package
 #git clone -b v5 https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
 
 rm -rf package/xiaorouji/luci-app-passwall2/luci-app-passwall2/htdocs
-#git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
-#git clone https://github.com/sirpdboy/luci-app-advancedplus package/sirpdboy/luci-app-advancedplus
+git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
 
 
