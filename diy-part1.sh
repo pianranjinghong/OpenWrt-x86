ce0c9ea6a33e84f0c2df624a7c083fd6580f6194
pianranjinghong <806481345@qq.com>
2025-11-23 17:24:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,10 +15,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-git clone https://github.com/kiddin9/luci-theme-edge package/kiddin9/luci-theme-edge
-#git clone https://github.com/jerrykuku/luci-theme-argon feeds/luci/themes/luci-theme-argon
-#git clone -b js https://github.com/sirpdboy/luci-theme-kucat package/sirpdboy/luci-theme-kucat
-
 # ------------------PassWall 科学上网--------------------------
 # 移除 openwrt feeds 自带的核心库
 #rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,pdnsd-alt,brook,chinadns-ng,dns2socks,dns2tcp,gn,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan,trojan-go,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,gn}
@@ -26,11 +22,11 @@ git clone https://github.com/kiddin9/luci-theme-edge package/kiddin9/luci-theme-
 #git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 
 #rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/packages/net/{v2ray-geodata,mosdns}
+rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/applications/luci-app-smartdns
 
-git clone https://github.com/lwb1978/openwrt-smartdns.git feeds/packages/net/smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
 git clone https://github.com/pianranjinghong/v2ray-geodata feeds/packages/net/v2ray-geodata
 #git clone https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/xiaorouji/luci-app-passwall2
