aef7aec668699cf599554478c1d05f6360bc12b9
pianranjinghong <806481345@qq.com>
2025-11-24 15:36:06 +0800

    Comment out luci-app-smartdns clone command
    Comment out the git clone command for luci-app-smartdns.

--- diy-part1.sh
+++ diy-part1.sh
@@ -34,6 +34,5 @@ git clone -b openwrt https://github.com/pianranjinghong/openwrt-packages package
 #git clone -b v5 https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
 
 rm -rf package/xiaorouji/luci-app-passwall2/luci-app-passwall2/htdocs
-git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
-
+#git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
 
