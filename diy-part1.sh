a6cebc0f218cf3f943b134026141967194c161fe
pianranjinghong <806481345@qq.com>
2026-02-23 09:34:43 +0800

    Update repository URLs and paths in diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,11 +28,11 @@ rm -rf feeds/packages/net/v2ray-geodata
 
 #git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
 git clone https://github.com/pianranjinghong/v2ray-geodata feeds/packages/net/v2ray-geodata
-#git clone https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2 package/xiaorouji/luci-app-passwall2
+#git clone https://github.com/Openwrt-Passwall/openwrt-passwall feeds/luci/applications/luci-app-passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 package/Openwrt-Passwall/openwrt-passwall2
 git clone -b openwrt https://github.com/pianranjinghong/openwrt-packages package/pianranjinghong
 #git clone -b v5 https://github.com/sbwml/luci-app-mosdns package/sbwml/luci-app-mosdns
 
-rm -rf package/xiaorouji/luci-app-passwall2/luci-app-passwall2/htdocs
+rm -rf package/Openwrt-Passwall/openwrt-passwall2/luci-app-passwall2/htdocs
 #git clone https://github.com/pymumu/luci-app-smartdns feeds/luci/applications/luci-app-smartdns
 
